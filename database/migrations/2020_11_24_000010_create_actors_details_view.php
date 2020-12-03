faa238807d11754e24601d700b01187cbd883b3a
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2020-12-03 14:37:02 -0500

    Fix  view query

--- database/migrations/2020_11_24_000010_create_actors_details_view.php
+++ database/migrations/2020_11_24_000010_create_actors_details_view.php
@@ -19,39 +19,26 @@ class CreateActorsDetailsView extends Migration {
      */
     public function up (): void {
         DB::statement(/** @lang MySQL */ "
-CREATE VIEW `actors_details` AS 
-SELECT `actors`.`id`     AS `id`,
-       `ac`.`ancestor_id`            AS `parent_id`,
-       COALESCE(`asp`.`is_group`, 0) AS `parent_is_group`,
-       COUNT(`acc`.`descendant_id`)  AS `direct_children_count`,
-       COALESCE((SELECT GROUP_CONCAT(`a`.`name` ORDER BY `ac`.`depth` DESC SEPARATOR '/')
-                   FROM `actors`               `a`
-                        JOIN `actors_closures` `ac`
-                             ON `ac`.`ancestor_id` = `a`.`id`
-	                             AND `a`.`is_group` = 1
-                  WHERE `ac`.`descendant_id` = `a`.`id`
-                    AND `ac`.`ancestor_id` <> `a`.`id`
-                  GROUP BY `ac`.`descendant_id`
-                ), '')               AS `path_names`,
-       COALESCE((SELECT GROUP_CONCAT(`a`.`id` ORDER BY `ac`.`depth` DESC SEPARATOR '/')
-                   FROM `actors`               `a`
-                        JOIN `actors_closures` `ac`
-                             ON `ac`.`ancestor_id` = `a`.`id` AND `a`.`is_group` = 1
-                  WHERE `ac`.`descendant_id` = `a`.`id`
-                    AND `ac`.`ancestor_id` <> `a`.`id`
-                  GROUP BY `ac`.`descendant_id`
-                ), '')               AS `path_ids`
-	FROM `actors`
-	     LEFT JOIN `actors_closures` `ac`
-	               ON `ac`.`descendant_id` = `actors`.`id`
-		               AND `ac`.`depth` = 1
-	     LEFT JOIN `actors_closures` `acc`
-	               ON `acc`.`ancestor_id` = `actors`.`id`
-		               AND `acc`.`depth` > 0
-	     LEFT JOIN `actors`          `asp`
-	               ON `asp`.`id` = `ac`.`ancestor_id`
- GROUP BY `actors`.`id`,
-          `ac`.`ancestor_id`
+CREATE VIEW `actors_details` AS
+SELECT `a`.`id`                                       AS `id`,
+       `ac`.`ancestor_id`                             AS `parent_id`,
+       COALESCE(`asp`.`is_group`, 0)                  AS `parent_is_group`,
+       COUNT(`acc`.`descendant_id`)                   AS `direct_children_count`,
+       COALESCE((SELECT GROUP_CONCAT(`apn`.`name` ORDER BY `ac`.`depth` DESC SEPARATOR '/')
+                   FROM (`neo_ooh`.`actors` `apn`
+                            JOIN `neo_ooh`.`actors_closures` `ac`
+                                 ON (((`ac`.`ancestor_id` = `apn`.`id`) AND (`ac`.`depth` > 0))))
+                  WHERE ((`ac`.`descendant_id` = `a`.`id`) AND (`apn`.`is_group` = 1))
+                  GROUP BY `ac`.`descendant_id`), '') AS `path_names`,
+       COALESCE((SELECT GROUP_CONCAT(`apn`.`id` ORDER BY `ac`.`depth` DESC SEPARATOR '/')
+                   FROM (`neo_ooh`.`actors` `apn`
+                            JOIN `neo_ooh`.`actors_closures` `ac`
+                                 ON (((`ac`.`ancestor_id` = `apn`.`id`) AND (`ac`.`depth` > 0))))
+                  WHERE ((`ac`.`descendant_id` = `a`.`id`) AND (`apn`.`is_group` = 1))
+                  GROUP BY `ac`.`descendant_id`), '') AS `path_ids`
+  FROM (((`neo_ooh`.`actors` `a` LEFT JOIN `neo_ooh`.`actors_closures` `ac` ON (((`ac`.`descendant_id` = `a`.`id`) AND (`ac`.`depth` = 1)))) LEFT JOIN `neo_ooh`.`actors_closures` `acc` ON (((`acc`.`ancestor_id` = `a`.`id`) AND (`acc`.`depth` = 1))))
+           LEFT JOIN `neo_ooh`.`actors` `asp` ON ((`asp`.`id` = `ac`.`ancestor_id`)))
+ GROUP BY `a`.`id`, `ac`.`ancestor_id`
         ");
     }
 

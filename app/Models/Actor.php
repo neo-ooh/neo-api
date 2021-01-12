8905f11bd9a233ba9d5c888542f731b958382158
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-01-12 15:58:48 -0500

    WiP 2021-01-11

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -447,7 +447,7 @@ class Actor extends SecuredModel implements AuthenticatableContract, Authorizabl
 
         // Libraries of the parent of the user
         if($parent && $this->parent_is_group) {
-            $libraries = $libraries->merge($this->parent->getLibraries(true, true, !$this->is_group && \Illuminate\Support\Facades\Gate::allows(\Neo\Enums\Capability::libraries_edit)));
+            $libraries = $libraries->merge($this->parent->getLibraries(true, true, !$this->is_group));
         }
 
         return $libraries->unique("id")->values();

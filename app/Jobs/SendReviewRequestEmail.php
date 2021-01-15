80293ee22b0c25af84d8069b30b45c7a83dc1e28
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-01-15 13:13:24 -0500

    2021-01-14

--- app/Jobs/SendReviewRequestEmail.php
+++ app/Jobs/SendReviewRequestEmail.php
@@ -68,6 +68,7 @@ class SendReviewRequestEmail implements ShouldQueue {
         // We need to determine who is responsible for reviewing this schedule
         $reviewers = $this->getReviewers($schedule);
 
+        Log::error($reviewers->pluck("email"));
         $reviewers->each(fn($reviewer) =>
             Mail::to($reviewer)->send(new ReviewRequestEmail($schedule))
         );

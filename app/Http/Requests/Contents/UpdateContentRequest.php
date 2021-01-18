38874d477258b19362947e17b5517e0cf83100e6
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-01-18 13:41:51 -0500

    WiP 2021-01-18

--- app/Http/Requests/Contents/UpdateContentRequest.php
+++ app/Http/Requests/Contents/UpdateContentRequest.php
@@ -15,7 +15,7 @@ use Illuminate\Support\Facades\Gate;
 use Neo\Enums\Capability;
 use Neo\Rules\AccessibleLibrary;
 
-class ssUpdateContentRequest extends FormRequest {
+class UpdateContentRequest extends FormRequest {
     /**
      * Determine if the user is authorized to make this request.
      *

df873d295bb30b5fc8fa8ffd352c0310fabe593d
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-01-26 15:30:49 -0500

    Fixing campaign access through actors

--- app/Http/Controllers/ActorsCampaignsController.php
+++ app/Http/Controllers/ActorsCampaignsController.php
@@ -4,20 +4,23 @@ namespace Neo\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Neo\Http\Requests\ActorsCampaigns\ListActorCampaignsRequest;
+use Neo\Models\Actor;
 
 class ActorsCampaignsController extends Controller {
     public function index(ListActorCampaignsRequest $request): Response {
-        return new Response($request->route('actor')->getCampaigns(true, true, false, false)
-                                  ->loadMissing([
-                                      "format",
-                                      "locations",
-                                      "owner",
-                                      "shares",
-                                      "schedules",
-                                      "schedules.content",
-                                      "schedules.owner:id,name",
-                                      "trashedSchedules",
-                                      "trashedSchedules.content"])
-                                  ->append(["related_libraries"]));
+        return new Response(Actor::query()
+                                 ->findOrFail($this->route('actor'))
+                                 ->getCampaigns(true, true, false, false)
+                                 ->loadMissing([
+                                     "format",
+                                     "locations",
+                                     "owner",
+                                     "shares",
+                                     "schedules",
+                                     "schedules.content",
+                                     "schedules.owner:id,name",
+                                     "trashedSchedules",
+                                     "trashedSchedules.content"])
+                                 ->append(["related_libraries"]));
     }
 }

66fbd97ae83f72ad19e6dbbb0425d047f5db714e
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-02-03 19:35:39 -0500

    --fixup

--- app/Http/Controllers/ActorsCampaignsController.php
+++ app/Http/Controllers/ActorsCampaignsController.php
@@ -7,18 +7,19 @@ use Neo\Http\Requests\ActorsCampaigns\ListActorCampaignsRequest;
 
 class ActorsCampaignsController extends Controller {
     public function index(ListActorCampaignsRequest $request): Response {
-        return new Response($request->route('actor')
-                                    ->getCampaigns(true, true, false, false)
-                                    ->loadMissing([
-                                        "format",
-                                        "locations",
-                                        "owner",
-                                        "shares",
-                                        "schedules",
-                                        "schedules.content",
-                                        "schedules.owner:id,name",
-                                        "trashedSchedules",
-                                        "trashedSchedules.content"])
-                                    ->append(["related_libraries"]));
+        return new Response(Actor::query()
+                                 ->findOrFail($request->route('actor'))
+                                 ->getCampaigns(true, true, false, false)
+                                 ->loadMissing([
+                                     "format",
+                                     "locations",
+                                     "owner",
+                                     "shares",
+                                     "schedules",
+                                     "schedules.content",
+                                     "schedules.owner:id,name",
+                                     "trashedSchedules",
+                                     "trashedSchedules.content"])
+                                 ->append(["related_libraries"]));
     }
 }

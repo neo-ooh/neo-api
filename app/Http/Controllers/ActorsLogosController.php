dacd5b6077d846616784c686945e29ae846704d4
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-02-11 16:40:04 -0500

    fixup! fixup! fixup! fixup! fixup! fixup! Actors Logo support + multiple fixes

--- app/Http/Controllers/ActorsLogosController.php
+++ app/Http/Controllers/ActorsLogosController.php
@@ -4,8 +4,6 @@ namespace Neo\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Illuminate\Http\UploadedFile;
-use Illuminate\Support\Facades\Storage;
-use Intervention\Image\Facades\Image;
 use Neo\Http\Requests\ActorsLogos\DestroyActorLogoRequest;
 use Neo\Http\Requests\ActorsLogos\StoreActorLogoRequest;
 use Neo\Models\Actor;
@@ -26,13 +24,13 @@ class ActorsLogosController extends Controller {
         }
 
         // Remove the actor logo if one is already present
-        if($actor->logo !== null) {
+        if ($actor->logo !== null) {
             $actor->logo->erase();
         }
 
         // The request has already validated that the file is an image, but we want to make sure we only store pngs
-        $actorLogo = new ActorLogo();
-        $actorLogo->id = $actor->id;
+        $actorLogo                = new ActorLogo();
+        $actorLogo->id            = $actor->id;
         $actorLogo->original_name = $uploadedFile->getClientOriginalName();
         $actorLogo->save();
         $actorLogo->refresh();
@@ -42,7 +40,7 @@ class ActorsLogosController extends Controller {
     }
 
     public function destroy(DestroyActorLogoRequest $request, Actor $actor) {
-        if($actor->logo !== null) {
+        if ($actor->logo !== null) {
             $actor->logo->erase();
         }
 

97a2007548a95317b276b332ff49781a0cc2f2ed
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-02-11 11:51:51 -0500

    fixup! fixup! Add a proper expiration date to the tokens

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -501,9 +501,6 @@ class Actor extends SecuredModel implements AuthenticatableContract, Authorizabl
             return true;
         }
 
-        // Delete the token
-        $token->delete();
-
         return false;
     }
 

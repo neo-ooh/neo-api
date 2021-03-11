f85ea9d9622b73dd18e17d827c3c28a8649436c5
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-03-11 13:20:47 -0500

    fixup! Add support for executing network refresh from the API

---
+++ app/Http/Controllers/NetworkController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Neo\Http\Controllers;
+
+use Illuminate\Support\Facades\Artisan;
+use Neo\Http\Requests\Network\NetworkRequest;
+
+class NetworkController extends Controller
+{
+    public function refresh(NetworkRequest $request) {
+        Artisan::queue("network:refresh");
+    }
+}

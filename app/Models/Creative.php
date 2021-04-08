8f37b0c460de5d773fb4f94e0b3f9701b97011a3
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-04-08 14:35:03 -0400

    WiP 2021-04-06

--- app/Models/Creative.php
+++ app/Models/Creative.php
@@ -231,7 +231,7 @@ class Creative extends Model {
             $constraint->upsize();
         });
 
-        Storage::put($this->thumbnail_path, $img->stream("jpg", 75));
+        Storage::put($this->thumbnail_path, $img->stream("jpg", 75)->__toString());
     }
 
 

ef6892e85ff279fc5cbc8bf0ae6b38b79b304b15
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-04-19 11:30:25 -0400

    WiP 2021-04-19

--- app/Http/Requests/NewsBackgrounds/ListBackgroundsRequest.php
+++ app/Http/Requests/NewsBackgrounds/ListBackgroundsRequest.php
@@ -26,7 +26,7 @@ class ListBackgroundsRequest extends FormRequest
     public function rules()
     {
         return [
-            "network" => ["sometimes", "nullable", "string"],
+            "network" => ["sometimes", "nullable", "integer"],
             "format_id" => ["sometimes", "integer", "exists:formats,id"],
             "locale" => ["sometimes", "string"]
         ];

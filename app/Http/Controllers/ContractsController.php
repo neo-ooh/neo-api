7400662c2449a74a4031aebbc6fb88787a37c350
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-05-06 10:08:18 -0400

    WiP 2021-05-06

--- app/Http/Controllers/ContractsController.php
+++ app/Http/Controllers/ContractsController.php
@@ -4,10 +4,13 @@ namespace Neo\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Neo\Http\Requests\Contracts\ShowContractRequest;
+use Neo\Http\Requests\Contracts\StoreContractRequest;
+use Neo\Models\Client;
 use Neo\Models\Contract;
 
 class ContractsController extends Controller {
-    public function store() {
+    public function store(StoreContractRequest $request) {
+        $contractId = $request->input("contract_id");
     }
 
     public function show(ShowContractRequest $request, Contract $contract) {
---
+++ app/Http/Requests/Contracts/StoreContractRequest.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Neo\Http\Requests\Contracts;
+
+use Auth;
+use Gate;
+use Illuminate\Foundation\Http\FormRequest;
+use Neo\Enums\Capability;
+use Neo\Models\Contract;
+
+class StoreContractRequest extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     *
+     * @return bool
+     */
+    public function authorize()
+    {
+        /** @var Contract $contract */
+        return Gate::allows(Capability::contracts_edit);
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array
+     */
+    public function rules()
+    {
+        return [
+            "contract_id" => ["required", "string"],
+            "client_id" => ["required", "exists:clients,id"],
+        ];
+    }
+}

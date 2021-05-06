18c0b18ca2798e4451dd8dde29899e5654c658da
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-05-06 16:46:11 -0400

    WiP 2021-05-06

--- resources/views/documents/contract/footer.blade.php
+++ resources/views/documents/contract/footer.blade.php
@@ -1,19 +1,19 @@
 <footer>
-    <table class="footer">
+    <table class="footer-table">
         <tr>
             <td class="footer-logo">
                 <img class="footer-company-branding-logo"
-                     src="{{ resource_path('/logos/shopping.dark@2x.png')  }}" alt="Neo-Shopping logo"/>
+                     src="{{ resource_path('/logos/shopping.dark@2x.png')  }}" alt="Neo-Shopping logo" width="25mm"/>
             </td>
             <td class="footer-logo">
                 <img class="footer-company-branding-logo"
-                     src="{{ resource_path('/logos/onthego.dark@2x.png')  }}" alt="Neo-On the Go logo"/>
+                     src="{{ resource_path('/logos/onthego.dark@2x.png')  }}" alt="Neo-On the Go logo" width="25mm"/>
             </td>
             <td class="footer-logo">
                 <img class="footer-company-branding-logo"
-                     src="{{ resource_path('/logos/fitness.dark@2x.png')  }}" alt="Neo-Fitness logo"/>
+                     src="{{ resource_path('/logos/fitness.dark@2x.png')  }}" alt="Neo-Fitness logo" width="25mm"/>
             </td>
-            <td class="footer-folio" style="width: {{ $width - 80 }}mm">
+            <td class="footer-folio" >
                 <span class="footer-folio-current">{PAGENO}</span>
                 <span class="footer-folio-group">/{nbpg}</span>
             </td>

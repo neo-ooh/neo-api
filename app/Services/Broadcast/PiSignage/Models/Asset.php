d79f5d67425eac5d6c4504d10e6b96644acf67c3
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-05-25 17:26:04 -0400

    WiP 2021-05-25

--- app/Services/Broadcast/PiSignage/Models/Asset.php
+++ app/Services/Broadcast/PiSignage/Models/Asset.php
@@ -12,12 +12,12 @@ use Neo\Services\Broadcast\PiSignage\API\PiSignageClient;
  *
  * @package Neo\Services\Broadcast\PiSignage\Models
  *
- * @property string   $name
- * @property string   $size
- * @property string   $ctime
- * @property string   $path
- * @property string   $type
- * @property string   $dbdata
+ * @property string $name
+ * @property string $size
+ * @property string $ctime
+ * @property string $path
+ * @property string $type
+ * @property string $dbdata
  *
  */
 class Asset extends PiSignageModel {
@@ -33,23 +33,21 @@ class Asset extends PiSignageModel {
 
     protected static function actions(): array {
         return [
-            "all"    => Endpoint::get("/files")->parser(new MultipleResourcesParser(static::class)),
+            "all"          => Endpoint::get("/files")->parser(new MultipleResourcesParser(static::class)),
             "createStatic" => Endpoint::post("/files")
-                                ->multipart()
-                                ->parser(new SingleResourcesParser(static::class)),
-            "get"    => Endpoint::get("/files/{id}")->parser(new SingleResourcesParser(static::class)),
-            "update" => Endpoint::post("/files/{id}")->parser(new SingleResourcesParser(static::class)),
-            "delete" => Endpoint::post("/files/{id}"),
+                                      ->multipart()
+                                      ->parser(new SingleResourcesParser(static::class)),
+            "get"          => Endpoint::get("/files/{id}")->parser(new SingleResourcesParser(static::class)),
+            "update"       => Endpoint::post("/files/{id}")->parser(new SingleResourcesParser(static::class)),
+            "delete"       => Endpoint::post("/files/{id}"),
         ];
     }
 
     public static function makeStatic(PiSignageClient $client, string $filename, $file_content) {
         return static::createStatic($client, [
-            "multipart" => [[
-                "name" => "assets",
-                "contents" => $file_content,
-                "filename" => $filename
-            ]]
+            "name"     => "assets",
+            "contents" => $file_content,
+            "filename" => $filename
         ]);
     }
 }

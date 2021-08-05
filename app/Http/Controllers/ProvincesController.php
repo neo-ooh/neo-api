e937aa572b102862ea88667464a9c3365e7a17af
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-08-05 14:44:54 -0400

    WiP 2021-08-05

--- app/Http/Controllers/ProvincesController.php
+++ app/Http/Controllers/ProvincesController.php
@@ -5,13 +5,14 @@ namespace Neo\Http\Controllers;
 use Illuminate\Http\Response;
 use Neo\Http\Requests\Provinces\ListProvincesRequest;
 use Neo\Models\Country;
+use Neo\Models\Province;
 
 class ProvincesController extends Controller {
     public function index(ListProvincesRequest $request, Country $country) {
         return new Response($country->provinces->load(["markets"]));
     }
 
-    public function show(ListProvincesRequest $request, Country $country) {
-        return new Response($country->provinces->load(["markets", "cities"]));
+    public function show(ListProvincesRequest $request, Country $country, Province $province) {
+        return new Response($province->load(["markets", "cities"]));
     }
 }

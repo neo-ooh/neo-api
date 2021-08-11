dab07ab05c8c10a97f6919db4b95255677b47ea6
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-08-11 16:52:13 -0400

    block telescope access from outside local env

--- app/Providers/TelescopeServiceProvider.php
+++ app/Providers/TelescopeServiceProvider.php
@@ -65,7 +65,9 @@ class TelescopeServiceProvider extends TelescopeApplicationServiceProvider
     protected function gate()
     {
         Gate::define('viewTelescope', function ($user) {
-            return true;
+            return in_array($user->email, [
+                //
+            ]);
         });
     }
 }

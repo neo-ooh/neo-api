06d56a5d3d7ef5135502629347335f3944a3b41a
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-09-24 13:27:24 -0400

    WiP 2021-09-24

---
+++ app/Jobs/Odoo/SendContactFlightJob.php
@@ -0,0 +1,111 @@
+<?php
+/*
+ * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * Written by Valentin Dufois <vdufois@neo-ooh.com>
+ *
+ * @neo/api - SendContactFlightJob.php
+ */
+
+namespace Neo\Jobs\Odoo;
+
+use Carbon\Carbon;
+use Illuminate\Bus\Queueable;
+use Illuminate\Queue\SerializesModels;
+use Illuminate\Queue\InteractsWithQueue;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Foundation\Bus\Dispatchable;
+use Neo\Models\Odoo\ProductCategory;
+use Neo\Models\Odoo\ProductType;
+use Neo\Models\Property;
+use Neo\Services\Odoo\Models\Campaign;
+use Neo\Services\Odoo\Models\Contract;
+use Neo\Services\Odoo\Models\OrderLine;
+use Neo\Services\Odoo\Models\Product;
+use Neo\Services\Odoo\OdooConfig;
+
+class SendContactFlightJob implements ShouldQueue {
+    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+
+    public function __construct(protected Contract $contract, protected array $flight) {
+
+    }
+
+    public function handle() {
+        clock()->event("Send Flight")->begin();
+
+        $client = OdooConfig::fromConfig()->getClient();
+
+        $flightType  = $this->flight["type"];
+        $flightStart = Carbon::parse($this->flight['start'])->toDateString();
+        $flightEnd   = Carbon::parse($this->flight['end'])->toDateString();
+
+        clock()->event("Create flight in Odoo")->color('red')->begin();
+
+        $campaign = clock(Campaign::create($client, [
+            "order_id"   => $this->contract->id,
+            "state"      => "draft",
+            "date_start" => $flightStart,
+            "date_end"   => $flightEnd,
+        ]));
+
+        clock()->event("Create flight in Odoo")->end();
+
+        // Preload the properties and products used by the flight
+        $properties = Property::with("odoo")->findMany(collect($this->flight["selection"])->pluck("0.0"));
+        $productsCategories = ProductCategory::findMany(collect($this->flight["selection"])->pluck("0.1"));
+
+        // Now we need to add each specified product
+        foreach ($this->flight["selection"] as $selection) {
+            $key =  implode(",", $selection[0]);
+            clock()->event("Handle product #$key")->begin();
+
+            [$propertyId, $productId] = $selection[0];
+            clock([$propertyId, $productId]);
+
+            // We need the property and product record from Connect
+            /** @var Property $connectProperty */
+            $connectProperty = $properties->firstOrFail(fn($property) => $property->getKey()  ===  $propertyId);
+            /** @var ProductType $connectProduct */
+            $connectProduct = $productsCategories->firstOrFail(fn($product) => $product->getKey()  ===  $productId);
+
+            clock()->event("Request matching products from Odoo Property #$key")->color('red')->begin();
+            // Pull the products of the odoo property matching the product type
+            $products = Product::all($client, [
+                ["shopping_center_id", "=", $connectProperty->odoo->odoo_id],
+                ["categ_id", "=", $connectProduct->odoo_id]
+            ]);
+
+            clock()->event("Request matching products from Odoo Property #$key")->end();
+
+            // Filter products based on flight type
+            if ($flightType === 'bua') {
+                $products = $products->where("bonus", "=", true);
+            } else {
+                $products = $products->where("bonus", "=", false);
+            }
+
+            /** @var Product $product */
+            foreach ($products as $product) {     // Add a new order line with the first product
+                clock()->event("$key -> $product->name")->color('red')->begin();
+                OrderLine::create($client, [
+                    "order_id"        => $this->contract->id,
+                    "name"            => $product->name,
+                    "price_unit"      => $product->list_price,
+                    "product_uom_qty" => 1.0,
+                    "customer_lead"   => 0.0,
+                    "product_id"      => $product->product_variant_id[0],
+                    "rental_start"    => $flightStart,
+                    "rental_end"      => $flightEnd,
+                    "is_rental_line"  => 1,
+                    "discount"        => $flightType === 'bonus' ? 100.0 : 0.0
+                ]);
+                clock()->event("$key -> $product->name")->end();
+            }
+
+            clock()->event("Handle product #$key")->end();
+        }
+        clock()->event("Send Flight")->end();
+    }
+}
--- app/Jobs/Odoo/SendContractJob.php
+++ app/Jobs/Odoo/SendContractJob.php
@@ -34,88 +34,14 @@ class SendContractJob implements ShouldQueue {
     public function handle() {
         clock()->event('Send contract')->color('purple')->begin();
 
-        $client = OdooConfig::fromConfig()->getClient();
 
         // We parse each flight of the contract, if it should be sent, we create a campaign in odoo for it, and add all the required orderlines
-        foreach ($this->flights as $flightKey => $flight) {
-            clock()->event("Send Flight #".$flightKey)->begin();
-
+        foreach ($this->flights as $flight) {
             if (!$flight['send']) {
-                clock()->info("Flight #$flightKey is not marked for sending");
-                clock()->event("Send Flight #".$flightKey)->end();
                 continue;
             }
 
-            $flightType  = $flight["type"];
-            $flightStart = Carbon::parse($flight['start'])->toDateString();
-            $flightEnd   = Carbon::parse($flight['end'])->toDateString();
-
-            clock()->event("Create flight in Odoo")->color('red')->begin();
-
-            $campaign = clock(Campaign::create($client, [
-                "order_id"   => $this->contract->id,
-                "state"      => "draft",
-                "date_start" => $flightStart,
-                "date_end"   => $flightEnd,
-            ]));
-
-            clock()->event("Create flight in Odoo")->end();
-
-            // Preload the properties and products used by the flight
-            $properties = Property::with("odoo")->findMany(collect($flight["selection"])->pluck("0.0"));
-            $productsCategories = ProductCategory::findMany(collect($flight["selection"])->pluck("0.1"));
-
-            // Now we need to add each specified product
-            foreach ($flight["selection"] as $selection) {
-                $key =  implode(",", $selection[0]);
-                clock()->event("Handle product #$key")->begin();
-
-                [$propertyId, $productId] = $selection[0];
-                clock([$propertyId, $productId]);
-
-                // We need the property and product record from Connect
-                /** @var Property $connectProperty */
-                $connectProperty = $properties->firstOrFail(fn($property) => $property->getKey()  ===  $propertyId);
-                /** @var ProductType $connectProduct */
-                $connectProduct = $productsCategories->firstOrFail(fn($product) => $product->getKey()  ===  $productId);
-
-                clock()->event("Request matching products from Odoo Property #$key")->color('red')->begin();
-                // Pull the products of the odoo property matching the product type
-                $products = Product::all($client, [
-                    ["shopping_center_id", "=", $connectProperty->odoo->odoo_id],
-                    ["categ_id", "=", $connectProduct->odoo_id]
-                ]);
-
-                clock()->event("Request matching products from Odoo Property #$key")->end();
-
-                // Filter products based on flight type
-                if ($flightType === 'bua') {
-                    $products = $products->where("bonus", "=", true);
-                } else {
-                    $products = $products->where("bonus", "=", false);
-                }
-
-                /** @var Product $product */
-                foreach ($products as $product) {     // Add a new order line with the first product
-                    clock()->event("$key -> $product->name")->color('red')->begin();
-                    OrderLine::create($client, [
-                        "order_id"        => $this->contract->id,
-                        "name"            => $product->name,
-                        "price_unit"      => $product->list_price,
-                        "product_uom_qty" => 1.0,
-                        "customer_lead"   => 0.0,
-                        "product_id"      => $product->product_variant_id[0],
-                        "rental_start"    => $flightStart,
-                        "rental_end"      => $flightEnd,
-                        "is_rental_line"  => 1,
-                        "discount"        => $flightType === 'bonus' ? 100.0 : 0.0
-                    ]);
-                    clock()->event("$key -> $product->name")->end();
-                }
-
-                clock()->event("Handle product #$key")->end();
-            }
-            clock()->event("Send Flight #".$flightKey)->end();
+            SendContactFlightJob::dispatchSync($this->contract, $flight);
         }
 
         clock()->event('Send contract')->end();

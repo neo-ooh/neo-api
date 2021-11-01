aa42b03cedf09f9952d8b4a302af728f647fb3f5
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-11-01 10:21:14 -0400

    Set up redis connection for queues and cache

--- config/database.php
+++ config/database.php
@@ -113,21 +113,21 @@ return [
 
         'default' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_AUTH'),
+            'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_DB', '0'),
         ],
 
         'cache' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_AUTH'),
+            'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_CACHE_DB', '1'),
         ],
 
         'geocoding' => [
             'host' => env('REDIS_HOST', '127.0.0.1'),
-            'password' => env('REDIS_AUTH'),
+            'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
             'database' => env('REDIS_GEOCODING_DB', '2'),
         ],

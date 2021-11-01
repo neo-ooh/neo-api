28e4c795b0ecfb44d713e5978b4c9cb3eaaf2e8a
valentin-dufois <19367658+valentin-dufois@users.noreply.github.com>
2021-11-01 10:24:28 -0400

    WiP 2021-11-01

--- config/database.php
+++ config/database.php
@@ -115,7 +115,7 @@ return [
             'host' => env('REDIS_HOST', '127.0.0.1'),
             'password' => env('REDIS_PASSWORD'),
             'port' => env('REDIS_PORT', '6379'),
-            'database' => env('REDIS_DB', '0'),
+            'database' => env('REDIS_QUEUES_DB', '0'),
         ],
 
         'cache' => [

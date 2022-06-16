d9ade354a2aea300105726f3aa5cc2b78e55cbce
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2022-06-16 15:42:21 -0400

    documents: Fix OTG property with unit price below 1$ being taken as linked lines and ignored

--- app/Documents/Contract/ContractImporter.php
+++ app/Documents/Contract/ContractImporter.php
@@ -59,7 +59,7 @@ class ContractImporter {
                 continue;
             }
 
-            if ((int)$orderLine->unit_price === 0 && $orderLine->isNetwork(Network::NEO_OTG)) {
+            if ($orderLine->unit_price <= 0 && $orderLine->isNetwork(Network::NEO_OTG)) {
                 // -Dans le On the Go, nous avons lié les produits In Screen et Full Screen dans une même propriété. Pourquoi? Parce qu'ils ont le même inventaire. Exemple: il y a 15 spot de dispo. Si un client achète un Digital Full Screen, il reste donc 14 dispos. Il va donc aussi rester 14 dispo autant pour In Screen que pour le Full Screen. Ce sont deux produits differents dans le même écran.
                 //Donc, dans Odoo, lorsque j'ajoute, un Full screen (ou vice versa), ca l'ajoute aussi un in screen qui toutefois se n'a aucune valeurs dans cette propositions. Ainsi, dans le cas que ca arrive, il ne faut pas affiher le In screen. En plus, il ne doit pas faire partie des calculs sur la ligne de total.
                 //Maintenant, quel champ utilisé. Je crois que le meilleur champs serait: Order Lines/Unit Price. Lorsqu'il est à 0, on affiche pas. Note que ceci est exclusif à On the Go.

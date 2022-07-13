37c8baddb5936b1f7c6582224bc4d6ec40398fca
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2022-07-13 15:59:12 -0400

    Adjust migration script

--- Modules/Broadcast/Database/Migrations/2022_07_12_155343_assign_new_ids_to_campaigns.php
+++ Modules/Broadcast/Database/Migrations/2022_07_12_155343_assign_new_ids_to_campaigns.php
@@ -11,19 +11,25 @@
 use Illuminate\Database\Migrations\Migration;
 use Neo\Modules\Broadcast\Enums\BroadcastResourceType;
 use Neo\Modules\Broadcast\Models\BroadcastResource;
+use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
 return new class extends Migration {
     public function up() {
         // For each of the new broadcast resources (contents, schedules, campaigns), we assign new ids in the tmp columns, and apply the same in the tables referencing them.
 
-        $output = new ConsoleOutput();
-
-        // Schedules
+        // Campaigns
         $campaigns = \Illuminate\Support\Facades\DB::table("campaigns")->orderBy("id")->lazy(500);
 
+        $output   = new ConsoleOutput();
+        $progress = new ProgressBar($output);
+        $progress->setFormat("%current%/%max% [%bar%] %percent:3s%% %message%");
+        $progress->setMessage("");
+        $progress->start($campaigns->count());
+
         foreach ($campaigns as $campaign) {
-            $output->writeln("Handling Campaign #$campaign->id");
+            $progress->setMessage("Handling Campaign #$campaign->id");
+            $progress->advance();
 
             // Get a new ID for the resource
             $broadcastResource = BroadcastResource::query()->create([
@@ -48,5 +54,7 @@ return new class extends Migration {
                 "campaign_id_tmp" => $broadcastResource->getKey(),
             ]);
         }
+
+        $progress->finish();
     }
 };

6257a66761d9e30865d055c90e8e3a57c0083167
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2022-07-18 15:12:04 -0400

    WiP

--- Modules/Broadcast/Database/Migrations/2022_07_18_145140_remove_old_criteria_tables.php
+++ Modules/Broadcast/Database/Migrations/2022_07_18_145140_remove_old_criteria_tables.php
@@ -13,17 +13,11 @@ use Illuminate\Support\Facades\Schema;
 
 return new class extends Migration {
     public function up() {
-<<<<<<< HEAD
         Schema::table("formats_layouts", static function (\Illuminate\Database\Schema\Blueprint $table) {
             $table->dropConstrainedForeignId("trigger_id");
             $table->dropConstrainedForeignId("separation_id");
         });
         Schema::dropIfExists("frame_settings_broadsign");
-        Schema::dropIfExists("frame_settings_pisignage");
-=======
-        Schema::dropColumns("formats_layouts", ["trigger_id", "separation_id"]);
-        Schema::dropIfExists("frame_settings_broadsign");
->>>>>>> 6550a640 (Add tags migrations)
 
         Schema::dropIfExists("broadsign_triggers");
         Schema::dropIfExists("broadsign_separations");

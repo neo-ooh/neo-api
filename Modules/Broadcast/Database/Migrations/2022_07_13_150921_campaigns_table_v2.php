939d27dd35c59ae8acd894129b3c4420327ed325
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2022-07-18 15:01:41 -0400

    WiP

--- Modules/Broadcast/Database/Migrations/2022_07_13_150921_campaigns_table_v2.php
+++ Modules/Broadcast/Database/Migrations/2022_07_13_150921_campaigns_table_v2.php
@@ -15,22 +15,21 @@ use Illuminate\Support\Facades\Schema;
 return new class extends Migration {
     public function up(): void {
         Schema::table("campaigns", static function (Blueprint $table) {
-            /**/
-            $table->renameColumn("owner_id", "parent_id");
+//            $table->renameColumn("owner_id", "parent_id");
         });
 
         Schema::table('campaigns', static function (Blueprint $table) {
-            /*            $table->foreignId("creator_id")->nullable()->constrained("actors", "id")->cascadeOnUpdate()->nullOnDelete();
-
-                        $table->time("start_time")->after("start_date")->default("00:00:00");
-                        $table->time("end_time")->after("end_date")->default("23:59:00");
-
-                        $table->unsignedTinyInteger("broadcast_days")->after("end_time")->default(127);
-
-                        $table->renameColumn("loop_saturation", "occurrences_in_loop");
-
-                        $table->dropConstrainedForeignId("format_id");
-                        $table->dropColumn("external_id");*/
+//            $table->foreignId("creator_id")->nullable()->constrained("actors", "id")->cascadeOnUpdate()->nullOnDelete();
+//
+//            $table->time("start_time")->after("start_date")->default("00:00:00");
+//            $table->time("end_time")->after("end_date")->default("23:59:00");
+//
+//            $table->unsignedTinyInteger("broadcast_days")->after("end_time")->default(127);
+//
+//            $table->renameColumn("loop_saturation", "occurrences_in_loop");
+//
+//            $table->dropConstrainedForeignId("format_id");
+//            $table->dropColumn("external_id");
             $table->dropConstrainedForeignId("network_id");
         });
 

3c911dc8f8e0bfeb636b5a41ec68259bcd535c8b
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2022-07-18 15:03:43 -0400

    WiP

--- Modules/Broadcast/Database/Migrations/2022_07_18_140125_migrate_criterions_to_broadcast_tags.php
+++ Modules/Broadcast/Database/Migrations/2022_07_18_140125_migrate_criterions_to_broadcast_tags.php
@@ -12,38 +12,15 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Support\Facades\DB;
 use Neo\Modules\Broadcast\Enums\BroadcastResourceType;
 use Neo\Modules\Broadcast\Enums\BroadcastTagScope;
-<<<<<<< HEAD
-use Neo\Modules\Broadcast\Enums\BroadcastTagType;
-use Neo\Modules\Broadcast\Enums\ExternalResourceType;
-use Neo\Modules\Broadcast\Models\BroadcastResource;
-use Neo\Modules\Broadcast\Models\ExternalResource;
-use Neo\Modules\Broadcast\Models\StructuredColumns\ExternalResourceData;
-use Symfony\Component\Console\Helper\ProgressBar;
-use Symfony\Component\Console\Output\ConsoleOutput;
-
-return new class extends Migration {
-    public function up() {
-        $output = new ConsoleOutput();
-        $output->writeln("");
-=======
 use Neo\Modules\Broadcast\Enums\ExternalResourceType;
 use Neo\Modules\Broadcast\Models\BroadcastResource;
 use Neo\Modules\Broadcast\Models\ExternalResource;
 use Symfony\Component\Console\Helper\ProgressBar;
 use Symfony\Component\Console\Output\ConsoleOutput;
 
-function getProgressBar(ConsoleOutput $output) {
-    $progress = new ProgressBar($output);
-    $progress->setFormat("%current%/%max% [%bar%] %percent:3s%% %message%");
-    $progress->setMessage("");
-
-    return $progress;
-}
-
 return new class extends Migration {
     public function up() {
         $output = new ConsoleOutput();
->>>>>>> 6550a640 (Add tags migrations)
 
         /** @var object $broadsignProvider */
         $broadsignProvider = DB::table("broadcasters_connections")
@@ -54,11 +31,7 @@ return new class extends Migration {
         $triggers = DB::table("broadsign_triggers")->get();
 
         $output->writeln("Handling triggers");
-<<<<<<< HEAD
         $progress = $this->getProgressBar($output);
-=======
-        $progress = getProgressBar($output);
->>>>>>> 6550a640 (Add tags migrations)
         $progress->start($triggers->count());
 
         foreach ($triggers as $trigger) {
@@ -67,19 +40,11 @@ return new class extends Migration {
 
             // Insert the trigger in the tags table
             /** @var BroadcastResource $broadcastResource */
-<<<<<<< HEAD
-            $broadcastResource = BroadcastResource::query()->create(["type" => BroadcastResourceType::Tag]);
-
-            DB::table("broadcast_tags")->insert([
-                "id"      => $broadcastResource->getKey(),
-                "type"    => BroadcastTagType::Trigger,
-=======
             $broadcastResource = BroadcastResource::query()->create(["type" => BroadcastResourceType::Creative]);
 
             DB::table("broadcast_tags")->insert([
                 "id"      => $broadcastResource->getKey(),
                 "type"    => "trigger",
->>>>>>> 6550a640 (Add tags migrations)
                 "name_en" => $trigger->name,
                 "name_fr" => $trigger->name,
                 "scope"   => BroadcastTagScope::Layout->value,
@@ -87,34 +52,20 @@ return new class extends Migration {
 
             // Insert the external_id
             ExternalResource::query()->create([
-<<<<<<< HEAD
-                "resource_id"    => $broadcastResource->getKey(),
-                "broadcaster_id" => $broadsignProvider->id,
-                "type"           => ExternalResourceType::Tag,
-                "data"           => new ExternalResourceData([
-                    "external_id" => $trigger->broadsign_trigger_id
-                ])
-=======
                 "resource_id"    => $broadcastResource,
                 "broadcaster_id" => $broadsignProvider->id,
                 "type"           => ExternalResourceType::Tag,
                 "data"           => [
                     "external_id" => $trigger->broadsign_trigger_id
                 ]
->>>>>>> 6550a640 (Add tags migrations)
             ]);
 
             // List all the layout using the trigger, and add a reference to the newly created tag
             $layouts = DB::table("formats_layouts")->where("trigger_id", "=", $trigger->id)->get();
 
             foreach ($layouts as $layout) {
-<<<<<<< HEAD
-                DB::table("layout_broadcast_tags")->insert([
-                    "layout_id"        => $layout->id,
-=======
                 DB::table("format_layout_broadcast_tags")->insert([
                     "format_layout_id" => $layout->id,
->>>>>>> 6550a640 (Add tags migrations)
                     "broadcast_tag_id" => $broadcastResource->getKey(),
                 ]);
             }
@@ -126,11 +77,7 @@ return new class extends Migration {
         $separations = DB::table("broadsign_separations")->get();
 
         $output->writeln("Handling separations");
-<<<<<<< HEAD
         $progress = $this->getProgressBar($output);
-=======
-        $progress = getProgressBar($output);
->>>>>>> 6550a640 (Add tags migrations)
         $progress->start($separations->count());
 
         foreach ($separations as $separation) {
@@ -139,19 +86,11 @@ return new class extends Migration {
 
             // Insert the trigger in the tags table
             /** @var BroadcastResource $broadcastResource */
-<<<<<<< HEAD
-            $broadcastResource = BroadcastResource::query()->create(["type" => BroadcastResourceType::Tag]);
-
-            DB::table("broadcast_tags")->insert([
-                "id"      => $broadcastResource->getKey(),
-                "type"    => BroadcastTagType::Category,
-=======
             $broadcastResource = BroadcastResource::query()->create(["type" => BroadcastResourceType::Creative]);
 
             DB::table("broadcast_tags")->insert([
                 "id"      => $broadcastResource->getKey(),
                 "type"    => "separation",
->>>>>>> 6550a640 (Add tags migrations)
                 "name_en" => $separation->name,
                 "name_fr" => $separation->name,
                 "scope"   => BroadcastTagScope::Layout->value,
@@ -159,34 +98,20 @@ return new class extends Migration {
 
             // Insert the external_id
             ExternalResource::query()->create([
-<<<<<<< HEAD
-                "resource_id"    => $broadcastResource->getKey(),
-                "broadcaster_id" => $broadsignProvider->id,
-                "type"           => ExternalResourceType::Tag,
-                "data"           => new ExternalResourceData([
-                    "external_id" => $separation->broadsign_separation_id
-                ])
-=======
                 "resource_id"    => $broadcastResource,
                 "broadcaster_id" => $broadsignProvider->id,
                 "type"           => ExternalResourceType::Tag,
                 "data"           => [
                     "external_id" => $separation->broadsign_separation_id
                 ]
->>>>>>> 6550a640 (Add tags migrations)
             ]);
 
             // List all the layout using the trigger, and add a reference to the newly created tag
             $layouts = DB::table("formats_layouts")->where("separation_id", "=", $separation->id)->get();
 
             foreach ($layouts as $layout) {
-<<<<<<< HEAD
-                DB::table("layout_broadcast_tags")->insert([
-                    "layout_id"        => $layout->id,
-=======
                 DB::table("format_layout_broadcast_tags")->insert([
                     "format_layout_id" => $layout->id,
->>>>>>> 6550a640 (Add tags migrations)
                     "broadcast_tag_id" => $broadcastResource->getKey(),
                 ]);
             }
@@ -198,11 +123,7 @@ return new class extends Migration {
         $criteria = DB::table("broadsign_criteria")->get();
 
         $output->writeln("Handling criteria");
-<<<<<<< HEAD
         $progress = $this->getProgressBar($output);
-=======
-        $progress = getProgressBar($output);
->>>>>>> 6550a640 (Add tags migrations)
         $progress->start($separations->count());
 
         foreach ($criteria as $criterion) {
@@ -211,19 +132,11 @@ return new class extends Migration {
 
             // Insert the trigger in the tags table
             /** @var BroadcastResource $broadcastResource */
-<<<<<<< HEAD
-            $broadcastResource = BroadcastResource::query()->create(["type" => BroadcastResourceType::Tag]);
-
-            DB::table("broadcast_tags")->insert([
-                "id"      => $broadcastResource->getKey(),
-                "type"    => BroadcastTagType::Targeting,
-=======
             $broadcastResource = BroadcastResource::query()->create(["type" => BroadcastResourceType::Creative]);
 
             DB::table("broadcast_tags")->insert([
                 "id"      => $broadcastResource->getKey(),
                 "type"    => "criteria",
->>>>>>> 6550a640 (Add tags migrations)
                 "name_en" => $criterion->name,
                 "name_fr" => $criterion->name,
                 "scope"   => BroadcastTagScope::Frame->value,
@@ -231,21 +144,12 @@ return new class extends Migration {
 
             // Insert the external_id
             ExternalResource::query()->create([
-<<<<<<< HEAD
-                "resource_id"    => $broadcastResource->getKey(),
-                "broadcaster_id" => $broadsignProvider->id,
-                "type"           => ExternalResourceType::Tag,
-                "data"           => new ExternalResourceData([
-                    "external_id" => $criterion->broadsign_criteria_id
-                ])
-=======
                 "resource_id"    => $broadcastResource,
                 "broadcaster_id" => $broadsignProvider->id,
                 "type"           => ExternalResourceType::Tag,
                 "data"           => [
                     "external_id" => $criterion->broadsign_criteria_id
                 ]
->>>>>>> 6550a640 (Add tags migrations)
             ]);
 
             // List all the layout using the trigger, and add a reference to the newly created tag
@@ -253,11 +157,7 @@ return new class extends Migration {
 
             foreach ($frames as $frame) {
                 DB::table("frame_broadcast_tags")->insert([
-<<<<<<< HEAD
-                    "frame_id"         => $frame->frame_id,
-=======
                     "frame_id"         => $frame->id,
->>>>>>> 6550a640 (Add tags migrations)
                     "broadcast_tag_id" => $broadcastResource->getKey(),
                 ]);
             }
@@ -265,7 +165,6 @@ return new class extends Migration {
 
         $progress->finish();
     }
-<<<<<<< HEAD
 
     protected function getProgressBar(ConsoleOutput $output) {
         $progress = new ProgressBar($output);
@@ -274,6 +173,4 @@ return new class extends Migration {
 
         return $progress;
     }
-=======
->>>>>>> 6550a640 (Add tags migrations)
 };

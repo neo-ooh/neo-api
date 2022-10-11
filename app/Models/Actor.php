7d6ea31bf6c68f18b175bf244b1121e150ddfaa5
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2022-10-11 14:47:09 -0400

    libraries: Adjust `getLibraries` to prevent loading libraries more than one level higher than the user

--- app/Models/Actor.php
+++ app/Models/Actor.php
@@ -502,7 +502,7 @@ class Actor extends SecuredModel implements AuthenticatableContract, Authorizabl
         // Libraries of the parent of the user
 //        if ($parent && ($this->details->parent_is_group ?? false)) {  --  WTF is this not working ?????
         if ($parent && ($this->parent->is_group ?? false)) {
-            $libraries = $libraries->merge($this->parent->getLibraries(true, true, !$this->is_group && Gate::allows(\Neo\Enums\Capability::libraries_edit) && !$this->limited_access));
+            $libraries = $libraries->merge($this->parent->getLibraries(true, true, !$this->is_group && Gate::allows(\Neo\Enums\Capability::libraries_edit) && !$this->limited_access, false));
         }
 
         return $libraries->unique("id")->sortBy("name")->values();

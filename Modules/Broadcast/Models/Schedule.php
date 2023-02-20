fb12e29d54794eb632deff1bb77ea49c951e2a1e
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-02-20 11:23:49 -0500

    schedules: Fix error when getting a schedule's resource when it has no contents.

--- Modules/Broadcast/Models/Schedule.php
+++ Modules/Broadcast/Models/Schedule.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Copyright 2023 (c) Neo-OOH - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
@@ -264,7 +264,7 @@ class Schedule extends BroadcastResourceModel {
     public function toResource(): ScheduleResource {
         return new ScheduleResource(
             enabled       : $this->status === ScheduleStatus::Approved || $this->status === ScheduleStatus::Live,
-            name          : $this->campaign->name . " - " . $this->contents->first()->name,
+            name          : $this->campaign->name . " - " . $this->contents->first()?->name,
             start_date    : $this->start_date->toDateString(),
             start_time    : $this->start_time->toTimeString(),
             end_date      : $this->end_date->toDateString(),

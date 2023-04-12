78aa0ba16dcff97ae9277dfbdb53b7260f4ca443
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-04-12 16:28:20 -0400

    creatives: fix relations with trashed models

--- Modules/Broadcast/Models/Creative.php
+++ Modules/Broadcast/Models/Creative.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Copyright 2023 (c) Neo-OOH - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
@@ -139,14 +139,14 @@ class Creative extends BroadcastResourceModel {
      * @return BelongsTo<Actor, Creative>
      */
     public function owner(): BelongsTo {
-        return $this->belongsTo(Actor::class, 'owner_id', 'id');
+        return $this->belongsTo(Actor::class, 'owner_id', 'id')->withTrashed();
     }
 
     /**
      * @return BelongsTo<Content, Creative>
      */
     public function content(): BelongsTo {
-        return $this->belongsTo(Content::class, 'content_id', 'id');
+        return $this->belongsTo(Content::class, 'content_id', 'id')->withTrashed();
     }
 
     /**

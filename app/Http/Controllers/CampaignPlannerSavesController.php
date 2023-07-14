830b245eb9769507d227156218f68d039c7666d2
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-07-14 09:58:22 -0400

    Revert "campaign-planner: Move saved plans data from DB to file storage"
    This reverts commit 571e90f2f7472c185dc60378d02a433654a45416.

--- app/Http/Controllers/CampaignPlannerSavesController.php
+++ app/Http/Controllers/CampaignPlannerSavesController.php
@@ -12,15 +12,14 @@ namespace Neo\Http\Controllers;
 
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Storage;
 use Neo\Http\Requests\CampaignPlannerSaves\DestroySaveRequest;
 use Neo\Http\Requests\CampaignPlannerSaves\ListSavesRequest;
 use Neo\Http\Requests\CampaignPlannerSaves\ShareSaveRequest;
 use Neo\Http\Requests\CampaignPlannerSaves\StoreSaveRequest;
 use Neo\Http\Requests\CampaignPlannerSaves\UpdateSaveRequest;
+use Neo\Http\Resources\CampaignPlannerSaveResource;
 use Neo\Models\Actor;
 use Neo\Models\CampaignPlannerSave;
-use Neo\Resources\CampaignPlannerPlan\CampaignPlannerPlan;
 
 class CampaignPlannerSavesController {
     public function index(ListSavesRequest $request, Actor $actor) {
@@ -42,74 +41,53 @@ class CampaignPlannerSavesController {
 
         $query->orderBy("updated_at", 'desc');
 
-        return new Response($query->get()->makeHidden("data"), 200, [
+        return new Response($query->get(), 200, [
             "Content-Range" => "items $from-$to/$totalCount",
         ]);
     }
 
     public function store(StoreSaveRequest $request) {
-        $plan = CampaignPlannerPlan::from(json_decode($request->input("plan"), true));
-
-        $save           = new CampaignPlannerSave();
-        $save->actor_id = Auth::id();
-        $save->name     = $request->input("name");
-        $save->version  = $request->input("version");
-
-        $save->contract        = $plan->plan->odoo?->contract;
-        $save->client_name     = $plan->plan->odoo?->partnerName[1];
-        $save->advertiser_name = $plan->plan->odoo?->analyticAccountName !== null ? $plan->plan->odoo?->analyticAccountName[1] : null;
+        $save = new CampaignPlannerSave([
+                                            "actor_id" => Auth::user()->id,
+                                            "name"     => $request->input("_meta")["name"],
+                                            "data"     => [
+                                                "plan"  => $request->input("plan"),
+                                                "_meta" => $request->input("_meta"),
+                                            ],
+                                        ]);
 
         $save->save();
 
-        $plan->_meta->id       = $save->id;
-        $plan->_meta->uid      = $save->uid;
-        $plan->_meta->actor_id = $save->actor_id;
 
-        $save->storePlan(json_encode($plan, JSON_UNESCAPED_UNICODE));
-
-        return new Response($save, 201);
+        return new Response(new CampaignPlannerSaveResource($save), 201);
     }
 
     public function show(Actor $actor, CampaignPlannerSave $campaignPlannerSave) {
-        return new Response($campaignPlannerSave);
+        $campaignPlannerSave->data = $campaignPlannerSave->getPlan();
+        return new Response(new CampaignPlannerSaveResource($campaignPlannerSave));
     }
 
     public function update(UpdateSaveRequest $request, Actor $actor, CampaignPlannerSave $campaignPlannerSave) {
-        $campaignPlannerSave->name    = $request->input("name");
-        $campaignPlannerSave->version = $request->input("version");
-
-        $rawPlan = $request->input("plan");
-        $plan    = CampaignPlannerPlan::from(json_decode($rawPlan, true));
-
-        $campaignPlannerSave->contract        = $plan->plan->odoo?->contract;
-        $campaignPlannerSave->client_name     = $plan->plan->odoo?->partnerName[1];
-        $campaignPlannerSave->advertiser_name = $plan->plan->odoo?->analyticAccountName !== null ? $plan->plan->odoo?->analyticAccountName[1] : null;
-
+        $campaignPlannerSave->name = $request->input("_meta")["name"];
+        $campaignPlannerSave->data = [
+            "plan"  => $request->input("plan"),
+            "_meta" => $request->input("_meta"),
+        ];
         $campaignPlannerSave->save();
 
-        $campaignPlannerSave->storePlan($rawPlan);
-
-        return new Response($campaignPlannerSave);
+        return new Response(new CampaignPlannerSaveResource($campaignPlannerSave));
     }
 
     public function share(ShareSaveRequest $request, Actor $actor, CampaignPlannerSave $campaignPlannerSave) {
         $receivers = $request->input("actors");
 
         foreach ($receivers as $receiverId) {
-            $newSave                  = new CampaignPlannerSave();
-            $newSave->name            = $campaignPlannerSave->name;
-            $newSave->version         = $campaignPlannerSave->version;
-            $newSave->actor_id        = $receiverId;
-            $newSave->contract        = $campaignPlannerSave->contract;
-            $newSave->client_name     = $campaignPlannerSave->client_name;
-            $newSave->advertiser_name = $campaignPlannerSave->advertiser_name;
+            $newSave           = new CampaignPlannerSave();
+            $newSave->name     = $campaignPlannerSave->name;
+            $newSave->actor_id = $receiverId;
+            $newSave->data     = $campaignPlannerSave->getPlan();
             $newSave->save();
-
-            clock()->event("Copy to Actor #$receiverId")->color("purple")->begin();
-            Storage::disk("public")->copy($campaignPlannerSave->plan_path, $newSave->plan_path);
-            clock()->event("Copy to Actor #$receiverId")->end();
         }
-
 
         return new Response([]);
     }

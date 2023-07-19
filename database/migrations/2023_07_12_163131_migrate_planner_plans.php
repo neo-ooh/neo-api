1e2d4fd07757c9703983dd7cbc908874b3454bf3
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-07-19 11:36:11 -0400

    misc: typo

--- database/migrations/2023_07_12_163131_migrate_planner_plans.php
+++ database/migrations/2023_07_12_163131_migrate_planner_plans.php
@@ -33,7 +33,7 @@ return new class extends Migration {
                                           ->where("id", "=", $plan->getKey())
                                           ->update([
                                                        "uid"             => $plan->uid ?? Hashids::encode($plan->id),
-                                                       "vik km ersion"   => $plan->version ?? 0,
+                                                       "viersion"        => $plan->version ?? 0,
                                                        "contract"        => $plan->contract_id,
                                                        "client_name"     => $plan->client_name,
                                                        "advertiser_name" => $plan->advertiser_name,

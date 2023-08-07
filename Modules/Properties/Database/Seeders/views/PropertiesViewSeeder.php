162ee30dfd60c868e8b31a926223aa14b7c85e66
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-07 14:21:54 -0400

    misc: fix error in view creation

--- Modules/Properties/Database/Seeders/views/PropertiesViewSeeder.php
+++ Modules/Properties/Database/Seeders/views/PropertiesViewSeeder.php
@@ -14,19 +14,19 @@ use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\DB;
 
 class PropertiesViewSeeder extends Seeder {
-    public function run() {
-        $viewName = "properties_view";
+	public function run() {
+		$viewName = "properties_view";
 
-        DB::statement("DROP VIEW IF EXISTS $viewName");
+		DB::statement("DROP VIEW IF EXISTS $viewName");
 
-        DB::statement(/** @lang SQL */ <<<EOS
+		DB::statement(/** @lang SQL */ <<<EOS
         CREATE VIEW $viewName AS
         SELECT `p`.*,
             COUNT(`dv`.`id`) as 'demographic_variables_count'
         FROM `properties` `p`
-           LEFT JOIN `neo_ooh`.`demographic_values` `dv` ON `p`.`actor_id` = `dv`.`property_id`
+           LEFT JOIN `demographic_values` `dv` ON `p`.`actor_id` = `dv`.`property_id`
         GROUP BY `p`.`actor_id`        
         EOS
-        );
-    }
+		);
+	}
 }

de711e7b80d0e080d021428bd7160b2102f999f1
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-14 10:35:01 -0400

    misc

--- app/Console/Commands/Test/TestCommand.php
+++ app/Console/Commands/Test/TestCommand.php
@@ -11,7 +11,8 @@
 namespace Neo\Console\Commands\Test;
 
 use Illuminate\Console\Command;
-use Neo\Models\ContractLine;
+use Neo\Jobs\Traffic\EstimateWeeklyTrafficFromMonthJob;
+use Neo\Modules\Properties\Models\Property;
 use PhpOffice\PhpSpreadsheet\Reader\Exception;
 
 class TestCommand extends Command {
@@ -24,11 +25,32 @@ class TestCommand extends Command {
 	 * @throws Exception
 	 */
 	public function handle() {
-		/** @var ContractLine $line */
-		$line = ContractLine::query()->where("flight_id", "=", 28521)
-		                    ->where("product_id", "=", 1)
-		                    ->first();
+		$otgProperties = Property::fromQuery("
+			WITH `adapt_properties` AS (
+  SELECT `properties`.`actor_id` as 'id'
+  FROM `properties`
+  JOIN `actors_closures` ON `properties`.`actor_id` = `actors_closures`.`descendant_id`
+  WHERE `actors_closures`.`ancestor_id` = 1344
+    AND `actors_closures`.`depth` > 0
+)
+SELECT *
+  FROM `properties` `p`
+ WHERE `p`.`network_id` = 3
+   AND `p`.`actor_id` NOT IN (1277)
+   AND `p`.`actor_id` NOT IN (SELECT `id` FROM `adapt_properties`);
+		");
 
-		dump($line->campaigns()->get()->toArray());
+		$otgProperties->load("actor", "traffic.monthly_data");
+
+
+		foreach ($otgProperties as $k => $property) {
+			$this->output->section("#$k - " . $property->actor->name);
+			foreach ($property->traffic->monthly_data as $monthly_datum) {
+				$this->output->comment($monthly_datum->year . "-" . $monthly_datum->month);
+				
+				$j = new EstimateWeeklyTrafficFromMonthJob($property->getKey(), $monthly_datum->year, $monthly_datum->month);
+				$j->handle();
+			}
+		}
 	}
 }

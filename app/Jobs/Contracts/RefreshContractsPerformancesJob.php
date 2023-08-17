80ce20252ea0b981be2dcbfeb99d24e101a3a900
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-17 14:52:35 -0400

    contracts: Fix contract performances refresh counting BUA impressions

--- app/Jobs/Contracts/RefreshContractsPerformancesJob.php
+++ app/Jobs/Contracts/RefreshContractsPerformancesJob.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Copyright 2023 (c) Neo-OOH - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
@@ -21,40 +21,41 @@ use Illuminate\Support\Collection;
 use Illuminate\Support\Facades\Cache;
 use Neo\Models\Contract;
 use Neo\Models\ContractFlight;
+use Neo\Resources\Contracts\FlightType;
 
 class RefreshContractsPerformancesJob implements ShouldQueue {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-    public function __construct(protected int|null $contract_id = null) {
-    }
+	public function __construct(protected int|null $contract_id = null) {
+	}
 
-    public function handle() {
-        Cache::tags("contract-performances")->clear();
+	public function handle() {
+		Cache::tags("contract-performances")->clear();
 
-        // We refresh contract performances for contracts who still have flights running
-        $contracts = Contract::query()
-                             ->when($this->contract_id !== null, function (Builder $query) {
-                                 $query->where("id", "=", $this->contract_id);
-                             })
-                             ->when($this->contract_id === null, function (Builder $query) {
-                                 $query->whereRelation("flights", function (Builder $query) {
-                                     $query->where("end_date", ">", Carbon::now()->subDays(2));
-                                 });
-                             })
-                             ->with(["flights", "reservations"])
-                             ->get();
+		// We refresh contract performances for contracts who still have flights running
+		$contracts = Contract::query()
+		                     ->when($this->contract_id !== null, function (Builder $query) {
+			                     $query->where("id", "=", $this->contract_id);
+		                     })
+		                     ->when($this->contract_id === null, function (Builder $query) {
+			                     $query->whereRelation("flights", function (Builder $query) {
+				                     $query->where("end_date", ">", Carbon::now()->subDays(2));
+			                     });
+		                     })
+		                     ->with(["flights", "reservations"])
+		                     ->get();
 
-        /** @var Contract $contract */
-        foreach ($contracts as $contract) {
-            /** @var Collection<ContractFlight> $guaranteedFlights */
-            $guaranteedFlights = $contract->flights->where("type", "<>", "bua");
-            $guaranteedFlights->append("performances");
+		/** @var Contract $contract */
+		foreach ($contracts as $contract) {
+			/** @var Collection<ContractFlight> $guaranteedFlights */
+			$guaranteedFlights = $contract->flights->where("type", "<>", FlightType::BUA);
+			$guaranteedFlights->append("performances");
 
-            $contract->received_impressions = $guaranteedFlights->flatMap(fn(ContractFlight $flight) => $flight->performances)
-                                                                ->sum("impressions");
+			$contract->received_impressions = $guaranteedFlights->flatMap(fn(ContractFlight $flight) => $flight->performances)
+			                                                    ->sum("impressions");
 
-            $contract->save();
+			$contract->save();
 
-        }
-    }
+		}
+	}
 }

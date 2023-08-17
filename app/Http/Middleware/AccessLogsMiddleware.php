af1968aad67c05d80f8f59eb151127388f00a4c8
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-17 11:25:41 -0400

    monitoring: Add user identification

--- app/Http/Middleware/AccessLogsMiddleware.php
+++ app/Http/Middleware/AccessLogsMiddleware.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Copyright 2023 (c) Neo-OOH - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
@@ -14,35 +14,55 @@ use Closure;
 use Illuminate\Http\Request;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Log;
+use Neo\Models\Actor;
+use Sentry\State\Scope;
+use function Sentry\configureScope;
 
 class AccessLogsMiddleware {
-    public function handle(Request $request, Closure $next) {
-        $requestData = [
-            "env"     => config("app.env"),
-            "method"  => $request->getMethod(),
-            "path"    => $request->getPathInfo(),
-            "query"   => $request->query->all(),
-            "payload" => $request->json(),
-            "headers" => array_filter($request->headers->all(), static fn($k) => $k !== "authorization"),
-            "client"  => [
-                "ip"         => $request->getClientIp(),
-                "id"         => $request->user()->id ?? 0,
-                "name"       => $request->user()->name ?? '-',
-                "user-agent" => $request->userAgent(),
-            ]
-        ];
+	public function handle(Request $request, Closure $next) {
+		$requestData = [
+			"env"     => config("app.env"),
+			"method"  => $request->getMethod(),
+			"path"    => $request->getPathInfo(),
+			"query"   => $request->query->all(),
+			"payload" => $request->json(),
+			"headers" => array_filter($request->headers->all(), static fn($k) => $k !== "authorization"),
+			"client"  => [
+				"ip"         => $request->getClientIp(),
+				"id"         => $request->user()->id ?? 0,
+				"name"       => $request->user()->name ?? '-',
+				"user-agent" => $request->userAgent(),
+			],
+		];
 
-        /** @var Response $response */
-        $response = $next($request);
+		// Log to Sentry
+		configureScope(function (Scope $scope) use ($request) {
+			$user = $request->user();
 
-        if ($response instanceof Response) {
-            $requestData["response"] = [
-                "status" => $response->status(),
-            ];
-        }
+			if ($user) {
+				$scope->setUser([
+					                "id"         => $user->getKey(),
+					                "username"   => $user->name,
+					                "email"      => $user->email ?? null,
+					                "segment"    => $user instanceof Actor ? 'user' : 'access-token',
+					                "ip_address" => $request->userAgent(),
+				                ]);
+			}
+		});
 
-        Log::channel("activity")->info("connect.access", $requestData);
 
-        return $response;
-    }
+		/** @var Response $response */
+		$response = $next($request);
+
+		if ($response instanceof Response) {
+			$requestData["response"] = [
+				"status" => $response->status(),
+			];
+		}
+
+		Log::channel("activity")->info("connect.access", $requestData);
+
+
+		return $response;
+	}
 }

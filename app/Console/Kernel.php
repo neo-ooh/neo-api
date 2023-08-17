2c46eb8e582220b23d98eb546db3abdcd38a6dca
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-17 10:45:22 -0400

    Fix error in kernel

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -30,116 +30,115 @@ use Neo\Modules\Broadcast\Console\Commands\SynchronizeNetworkCommand;
 use Neo\Modules\Properties\Jobs\CreateTrafficSnapshotJob;
 
 class Kernel extends ConsoleKernel {
-    /**
-     * The Artisan commands provided by your application.
-     *
-     * @var array
-     */
-    protected $commands = [
-        TestCommand::class,
+	/**
+	 * The Artisan commands provided by your application.
+	 *
+	 * @var array
+	 */
+	protected $commands = [
+		TestCommand::class,
 
-        // contracts:update
-        RefreshContracts::class,
+		// contracts:update
+		RefreshContracts::class,
 
-        // contracts:clear-screenshots
-        DeleteOldScreenshots::class,
+		// contracts:clear-screenshots
+		DeleteOldScreenshots::class,
 
-        // property:pull-traffic {property}
-        PullPropertyTraffic::class,
+		// property:pull-traffic {property}
+		PullPropertyTraffic::class,
 
-        // Broadcast -------------------
+		// Broadcast -------------------
 
-        // network:sync
-        SynchronizeNetworkCommand::class,
-        FetchCampaignsPerformancesCommand::class,
+		// network:sync
+		SynchronizeNetworkCommand::class,
+		FetchCampaignsPerformancesCommand::class,
 
-        // Data -------------------
+		// Data -------------------
 
-        PopulateCensusDivisionsCommand::class,
-        PopulateCensusSubdivisionsCommand::class,
-        PopulateCensusFederalElectoralDistrictsCommand::class,
-        PopulateCensusForwardSortationAreasCommand::class,
-    ];
+		PopulateCensusDivisionsCommand::class,
+		PopulateCensusSubdivisionsCommand::class,
+		PopulateCensusFederalElectoralDistrictsCommand::class,
+		PopulateCensusForwardSortationAreasCommand::class,
+	];
 
-    /**
-     * Define the application's command schedule.
-     *
-     * @param Schedule $schedule
-     * @return void
-     */
-    protected function schedule(Schedule $schedule): void {
-        // Only register recurring tasks on the production environment
-        if (config('app.env') !== 'production') {
-            return;
-        }
+	/**
+	 * Define the application's command schedule.
+	 *
+	 * @param Schedule $schedule
+	 * @return void
+	 */
+	protected function schedule(Schedule $schedule): void {
+		// Only register recurring tasks on the production environment
+		if (config('app.env') !== 'production') {
+			return;
+		}
 
-        /* -----------------
-         * Every-minute tasks
-         */
+		/* -----------------
+		 * Every-minute tasks
+		 */
 
-        // Send screenshots requests to player
-        $schedule->job(SendScreenshotRequests::class)->everyMinute();
-
-
-        /* -----------------
-         * "More often that hourly but not every minute"
-         */
-        /*        $schedule->call(function (NewsService $news) {
-                    $news->updateRecords();
-                })->everyFifteenMinutes();*/
+		// Send screenshots requests to player
+		$schedule->job(SendScreenshotRequests::class)->everyMinute();
 
 
-        /* -----------------
-         * Hourly tasks
-         */
-
-        // Refresh Contracts performances
-        $schedule->command("contracts:update")->everyThreeHours();
-
-
-        /* -----------------
-         * Daily tasks
-         */
-
-        // Take a snapshot of properties' traffic
-        $schedule->job(CreateTrafficSnapshotJob::class)->dailyAt("01:00");
-
-        $schedule->command('properties:sync')->daily();
-        $schedule->command('contracts:clear-screenshots')->daily();
-
-        // End of schedule email
-        /*$schedule->job(NotifyEndOfSchedules::class)->weekdays()
-                 ->timezone('America/Toronto')
-                 ->at("06:00");*/
+		/* -----------------
+		 * "More often that hourly but not every minute"
+		 */
+		/*        $schedule->call(function (NewsService $news) {
+					$news->updateRecords();
+				})->everyFifteenMinutes();*/
 
 
-        /* -----------------
-         * Monthly tasks
-         */
+		/* -----------------
+		 * Hourly tasks
+		 */
 
-        // Pull traffic data for property with Linkett pairing
-        $schedule->job(PullLatestTrafficData::class)->monthly();
-        $schedule->job(FillMissingTrafficValueJob::class)->monthlyOn(2);
+		// Refresh Contracts performances
+		$schedule->command("contracts:update")->everyThreeHours();
 
-        // Send Reminder about traffic data to users
-        $schedule->job(TrafficRequiredReminder::class)->monthlyOn(7);
-    }
 
-    /**
-     * Get the timezone that should be used by default for scheduled events.
-     *
-     * @return DateTimeZone|string|null
-     */
-    protected function scheduleTimezone(): DateTimeZone|string|null {
-        return 'America/Toronto';
-    }
+		/* -----------------
+		 * Daily tasks
+		 */
 
-    /**
-     * Register the commands for the application.
-     *
-     * @return void
-     */
-    protected function commands(): void {
-        $this->load(__DIR__ . '/Commands');
-    }
+		// Take a snapshot of properties' traffic
+		$schedule->job(CreateTrafficSnapshotJob::class)->dailyAt("01:00");
+
+		$schedule->command('contracts:clear-screenshots')->daily();
+
+		// End of schedule email
+		/*$schedule->job(NotifyEndOfSchedules::class)->weekdays()
+				 ->timezone('America/Toronto')
+				 ->at("06:00");*/
+
+
+		/* -----------------
+		 * Monthly tasks
+		 */
+
+		// Pull traffic data for property with Linkett pairing
+		$schedule->job(PullLatestTrafficData::class)->monthly();
+		$schedule->job(FillMissingTrafficValueJob::class)->monthlyOn(2);
+
+		// Send Reminder about traffic data to users
+		$schedule->job(TrafficRequiredReminder::class)->monthlyOn(7);
+	}
+
+	/**
+	 * Get the timezone that should be used by default for scheduled events.
+	 *
+	 * @return DateTimeZone|string|null
+	 */
+	protected function scheduleTimezone(): DateTimeZone|string|null {
+		return 'America/Toronto';
+	}
+
+	/**
+	 * Register the commands for the application.
+	 *
+	 * @return void
+	 */
+	protected function commands(): void {
+		$this->load(__DIR__ . '/Commands');
+	}
 }

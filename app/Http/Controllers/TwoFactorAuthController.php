9867e7290ece72cf6c53b7046e905bd8a2bd59ae
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-21 16:26:19 -0400

    auth: Fix fatal error when asking to validate a 2fa token without providing one

--- app/Http/Controllers/TwoFactorAuthController.php
+++ app/Http/Controllers/TwoFactorAuthController.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Copyright 2023 (c) Neo-OOH - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
@@ -10,7 +10,6 @@
 
 namespace Neo\Http\Controllers;
 
-use Illuminate\Database\Eloquent\Model;
 use Illuminate\Http\Response;
 use Illuminate\Support\Facades\Auth;
 use Neo\Http\Requests\Actors\RecycleTwoFARequest;
@@ -20,62 +19,63 @@ use Neo\Models\Actor;
 use Neo\Models\TwoFactorToken;
 
 class TwoFactorAuthController extends Controller {
-    public function validateToken(TwoFactorValidationRequest $request): Response {
-        // Get the user token
-        /**
-         * @var TwoFactorToken
-         */
-        $token = Auth::user()->twoFactorToken;
+	public function validateToken(TwoFactorValidationRequest $request): Response {
+		// Get the user token
+		/**
+		 * @var TwoFactorToken
+		 */
+		$token = Auth::user()->twoFactorToken;
 
-        // There is no need to check the token format, it has already been validated by the `TwoFactorValidationRequest`
+		// There is no need to check the token format, it has already been validated by the `TwoFactorValidationRequest`
 
-        // Try to validate the given token against the stored one
-        if (!$token->validate($request->validated()['token'])) {
-            // Given token is erroneous
-            return new Response([
-                "code"    => "auth.bad-2fa-token",
-                "message" => "Provided two-factor token is invalid",
-            ],
-                403);
-        }
+		// Try to validate the given token against the stored one
+		if (!$token?->validate($request->validated()['token'])) {
+			// Given token is erroneous
+			return new Response([
+				                    "code"    => "auth.bad-2fa-token",
+				                    "message" => "Provided two-factor token is invalid",
+			                    ],
+			                    403);
+		}
 
-        // Good token
-        return new Response([
-            "token" => Auth::user()->getJWT(),
-        ]);
-    }
+		// Good token
+		return new Response([
+			                    "token" => Auth::user()->getJWT(),
+		                    ]);
+	}
 
-    /**
-     * Deletes the authentication second-step token and create a new one for the passed actor
-     * @param RecycleTwoFARequest $request
-     * @param Actor               $actor
-     * @return Response
-     */
-    public function recycle(RecycleTwoFARequest $request, Actor $actor): Response {
-        // Delete any Two Fa token of the user
-        $actor->twoFactorToken()->delete();
+	/**
+	 * Deletes the authentication second-step token and create a new one for the passed actor
+	 *
+	 * @param RecycleTwoFARequest $request
+	 * @param Actor               $actor
+	 * @return Response
+	 */
+	public function recycle(RecycleTwoFARequest $request, Actor $actor): Response {
+		// Delete any Two Fa token of the user
+		$actor->twoFactorToken()->delete();
 
-        // Create a new one
-        $token = new TwoFactorToken();
-        $token->actor()->associate($actor);
-        $token->save();
+		// Create a new one
+		$token = new TwoFactorToken();
+		$token->actor()->associate($actor);
+		$token->save();
 
-        // We're good, creating the new token has sent an email to the user
-        $token->makeVisible("token");
-        return new Response($token);
-    }
+		// We're good, creating the new token has sent an email to the user
+		$token->makeVisible("token");
+		return new Response($token);
+	}
 
-    public function forceValidateToken(ValidateTwoFaRequest $request, Actor $actor): Response {
-        $token = $actor->twoFactorToken;
+	public function forceValidateToken(ValidateTwoFaRequest $request, Actor $actor): Response {
+		$token = $actor->twoFactorToken;
 
-        if(!$token) {
-            // No token, do nothing
-            return new Response([]);
-        }
+		if (!$token) {
+			// No token, do nothing
+			return new Response([]);
+		}
 
-        $token->makeVisible("token");
-        $token->validate($token->token);
+		$token->makeVisible("token");
+		$token->validate($token->token);
 
-        return new Response($token);
-    }
+		return new Response($token);
+	}
 }

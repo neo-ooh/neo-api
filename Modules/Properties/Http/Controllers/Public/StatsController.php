932ac5f6c0e4a0bc18ba1990620c10e7eeaf0fd6
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-08-28 16:51:41 -0400

    stats: cache stats for 24 hours

--- Modules/Properties/Http/Controllers/Public/StatsController.php
+++ Modules/Properties/Http/Controllers/Public/StatsController.php
@@ -10,6 +10,7 @@
 
 namespace Neo\Modules\Properties\Http\Controllers\Public;
 
+use Cache;
 use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Response;
 use Neo\Http\Controllers\Controller;
@@ -19,68 +20,86 @@ use Neo\Modules\Properties\Models\Property;
 
 class StatsController extends Controller {
 	public function show(ShowStatsRequest $request) {
-		$properties = Property::query()
-		                      ->when($request->has("product_id"), function (Builder $query) use ($request) {
-			                      $query->whereHas("products", function (Builder $query) use ($request) {
-				                      $query->where("id", "=", $request->input("product_id"));
-			                      });
-		                      })
-		                      ->when($request->has("category_id"), function (Builder $query) use ($request) {
-			                      $query->whereHas("products", function (Builder $query) use ($request) {
-				                      $query->where("category_id", "=", $request->input("category_id"));
-			                      });
-		                      })
-		                      ->when($request->has("market_id"), function (Builder $query) use ($request) {
-			                      $query->whereHas("address", function (Builder $query) use ($request) {
-				                      $query->whereHas("city", function (Builder $query) use ($request) {
-					                      $query->where("market_id", "=", $request->input("market_id"));
-				                      });
-			                      });
-		                      })
-		                      ->where("is_sellable", "=", true)
-		                      ->whereHas("products", function (Builder $query) {
-			                      $query->where("is_bonus", "=", false);
-			                      $query->where("is_sellable", "=", true);
-		                      })
-		                      ->whereHas("address", function (Builder $query) {
-			                      $query->whereNotNull("geolocation");
-		                      })
-		                      ->with("traffic.weekly_data")
-		                      ->get();
+		$key = "public.stats";
 
-		$products = Product::query()
-		                   ->when($request->has("product_id"), function (Builder $query) use ($request) {
-			                   $query->where("id", "=", $request->input("product_id"));
-		                   })
-		                   ->when($request->has("category_id"), function (Builder $query) use ($request) {
-			                   $query->where("category_id", "=", $request->input("category_id"));
-		                   })
-		                   ->when($request->has("market_id"), function (Builder $query) use ($request) {
-			                   $query->whereHas("property", function (Builder $query) use ($request) {
-				                   $query->whereHas("address", function (Builder $query) use ($request) {
-					                   $query->whereHas("city", function (Builder $query) use ($request) {
-						                   $query->where("market_id", "=", $request->input("market_id"));
+		if ($request->has("market_id")) {
+			$key .= ".market-" . $request->input("market_id");
+		}
+
+		if ($request->has("category_id")) {
+			$key .= ".category-" . $request->input("category_id");
+		}
+
+		if ($request->has("product_id")) {
+			$key .= ".product-" . $request->input("product_id");
+		}
+
+		$stats = Cache::remember($key, 3600 * 24, function () use ($request) {
+			$properties = Property::query()
+			                      ->when($request->has("product_id"), function (Builder $query) use ($request) {
+				                      $query->whereHas("products", function (Builder $query) use ($request) {
+					                      $query->where("id", "=", $request->input("product_id"));
+				                      });
+			                      })
+			                      ->when($request->has("category_id"), function (Builder $query) use ($request) {
+				                      $query->whereHas("products", function (Builder $query) use ($request) {
+					                      $query->where("category_id", "=", $request->input("category_id"));
+				                      });
+			                      })
+			                      ->when($request->has("market_id"), function (Builder $query) use ($request) {
+				                      $query->whereHas("address", function (Builder $query) use ($request) {
+					                      $query->whereHas("city", function (Builder $query) use ($request) {
+						                      $query->where("market_id", "=", $request->input("market_id"));
+					                      });
+				                      });
+			                      })
+			                      ->where("is_sellable", "=", true)
+			                      ->whereHas("products", function (Builder $query) {
+				                      $query->where("is_bonus", "=", false);
+				                      $query->where("is_sellable", "=", true);
+			                      })
+			                      ->whereHas("address", function (Builder $query) {
+				                      $query->whereNotNull("geolocation");
+			                      })
+			                      ->with("traffic.weekly_data")
+			                      ->get();
+
+			$products = Product::query()
+			                   ->when($request->has("product_id"), function (Builder $query) use ($request) {
+				                   $query->where("id", "=", $request->input("product_id"));
+			                   })
+			                   ->when($request->has("category_id"), function (Builder $query) use ($request) {
+				                   $query->where("category_id", "=", $request->input("category_id"));
+			                   })
+			                   ->when($request->has("market_id"), function (Builder $query) use ($request) {
+				                   $query->whereHas("property", function (Builder $query) use ($request) {
+					                   $query->whereHas("address", function (Builder $query) use ($request) {
+						                   $query->whereHas("city", function (Builder $query) use ($request) {
+							                   $query->where("market_id", "=", $request->input("market_id"));
+						                   });
 					                   });
 				                   });
-			                   });
-		                   })
-		                   ->where("is_bonus", "=", false)
-		                   ->where("is_sellable", "=", true)
-		                   ->whereHas("property", function (Builder $query) use ($request) {
-			                   $query->where("is_sellable", "=", true);
-			                   $query->whereHas("address", function (Builder $query) {
-				                   $query->whereNotNull("geolocation");
-			                   });
-		                   })->get();
+			                   })
+			                   ->where("is_bonus", "=", false)
+			                   ->where("is_sellable", "=", true)
+			                   ->whereHas("property", function (Builder $query) use ($request) {
+				                   $query->where("is_sellable", "=", true);
+				                   $query->whereHas("address", function (Builder $query) {
+					                   $query->whereNotNull("geolocation");
+				                   });
+			                   })->get();
 
-		return new Response([
-			                    "properties_count" => $properties->count(),
-			                    "products_count"   => $products->count(),
-			                    "faces_count"      => $products->sum("quantity"),
-			                    "networks_count"   => $properties->pluck("network_id")->unique()->count(),
-			                    "traffic"          => $properties->flatMap(fn(Property $p) => $p->traffic->weekly_data->slice(-4)
-			                                                                                                          ->pluck("traffic"))
-			                                                     ->sum(),
-		                    ]);
+			return [
+				"properties_count" => $properties->count(),
+				"products_count"   => $products->count(),
+				"faces_count"      => $products->sum("quantity"),
+				"networks_count"   => $properties->pluck("network_id")->unique()->count(),
+				"traffic"          => $properties->flatMap(fn(Property $p) => $p->traffic->weekly_data->slice(-4)
+				                                                                                      ->pluck("traffic"))
+				                                 ->sum(),
+			];
+		});
+
+		return new Response($stats);
 	}
 }

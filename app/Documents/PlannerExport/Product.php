0b0202a72147bf264f9ea303c26667ab5f1ae6ef
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-09-18 11:33:05 -0400

    misc: Fix assignement error
    Fixes NEO-API-3K

--- app/Documents/PlannerExport/Product.php
+++ app/Documents/PlannerExport/Product.php
@@ -13,36 +13,36 @@ namespace Neo\Documents\PlannerExport;
 use Illuminate\Support\Collection;
 
 class Product {
-    public int $id;
-    public \Neo\Modules\Properties\Models\Product $product;
+	public int $id;
+	public \Neo\Modules\Properties\Models\Product $product;
 
-    public int $faces;
-    public float $spots;
-    public int $impressions;
-    public float $mediaValue;
-    public float $mediaInvestment;
-    public float $productionCost;
-    public float $unitPrice;
-    public float $price;
-    public int $isDiscounted;
-    public float $cpm;
+	public int $faces;
+	public float $spots;
+	public int $impressions;
+	public float $mediaValue;
+	public float $mediaInvestment;
+	public float $productionCost;
+	public float $unitPrice;
+	public float $price;
+	public int $isDiscounted;
+	public float $cpm;
 
-    public Collection $products;
+	public Collection $products;
 
-    public function __construct(array $compiledProduct, \Neo\Modules\Properties\Models\Product $product) {
-        $this->id = $compiledProduct["id"];
+	public function __construct(array $compiledProduct, \Neo\Modules\Properties\Models\Product $product) {
+		$this->id = $compiledProduct["id"];
 
-        $this->product = $product;
+		$this->product = $product;
 
-        $this->faces           = $compiledProduct["quantity"];
-        $this->spots           = $compiledProduct["spots"];
-        $this->impressions     = $compiledProduct["impressions"];
-        $this->mediaValue      = $compiledProduct["media_value"];
-        $this->mediaInvestment = $compiledProduct["discounted_media_value"];
-        $this->productionCost  = $compiledProduct["production_cost_value"];
-        $this->unitPrice       = $compiledProduct["unit_price"];
-        $this->price           = $compiledProduct["price"];
-        $this->isDiscounted    = $compiledProduct["isDiscounted"];
-        $this->cpm             = $compiledProduct["cpm"];
-    }
+		$this->faces           = $compiledProduct["quantity"];
+		$this->spots           = $compiledProduct["spots"];
+		$this->impressions     = $compiledProduct["impressions"] ?? 0;
+		$this->mediaValue      = $compiledProduct["media_value"];
+		$this->mediaInvestment = $compiledProduct["discounted_media_value"];
+		$this->productionCost  = $compiledProduct["production_cost_value"];
+		$this->unitPrice       = $compiledProduct["unit_price"];
+		$this->price           = $compiledProduct["price"];
+		$this->isDiscounted    = $compiledProduct["isDiscounted"];
+		$this->cpm             = $compiledProduct["cpm"];
+	}
 }

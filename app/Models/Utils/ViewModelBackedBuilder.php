269658d051384c98db82f8e4fdd74e73a959179c
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-09-27 16:55:09 -0400

    misc: Prevent error when using custom where stmt on `ViewModel`s

--- app/Models/Utils/ViewModelBackedBuilder.php
+++ app/Models/Utils/ViewModelBackedBuilder.php
@@ -18,102 +18,102 @@ use Illuminate\Database\Eloquent\Model;
  *
  */
 class ViewModelBackedBuilder extends Builder {
-    public function setModel(Model $model) {
-        parent::setModel($model);
+	public function setModel(Model $model) {
+		parent::setModel($model);
 
-        return $this;
-    }
+		return $this;
+	}
 
-    protected function setForRead(): void {
-        $this->query->from($this->model->getReadTable());
-    }
+	protected function setForRead(): void {
+		$this->query->from($this->model->getReadTable());
+	}
 
-    protected function setForWrite(): void {
-        $this->query->from($this->model->getWriteTable());
+	protected function setForWrite(): void {
+		$this->query->from($this->model->getWriteTable());
 
-        // Relations store the table name with the column name
-        // We therefore need to rename the where who uses the read table to the write table
-        // otherwise actions on the related lines `->related()->delete()` will have columns referencing
-        // the read table
-        $wheres      = $this->query->wheres;
-        $writeWheres = [];
+		// Relations store the table name with the column name
+		// We therefore need to rename the where who uses the read table to the write table
+		// otherwise actions on the related lines `->related()->delete()` will have columns referencing
+		// the read table
+		$wheres      = $this->query->wheres;
+		$writeWheres = [];
 
-        foreach ($wheres as $where) {
-            if (is_string($where["column"])) {
-                $writeWheres[] = [
-                    ...$where,
-                    "column" => str_replace($this->model->getReadTable(), $this->model->getWriteTable(), $where["column"]),
-                ];
-            } else {
-                $writeWheres[] = $where;
-            }
-        }
+		foreach ($wheres as $where) {
+			if (array_key_exists("column", $where) && is_string($where["column"])) {
+				$writeWheres[] = [
+					...$where,
+					"column" => str_replace($this->model->getReadTable(), $this->model->getWriteTable(), $where["column"]),
+				];
+			} else {
+				$writeWheres[] = $where;
+			}
+		}
 
-        $this->query->wheres = $writeWheres;
-    }
+		$this->query->wheres = $writeWheres;
+	}
 
-    protected function writeStmt(Closure $callback) {
-        // Switch the builder `from` to the write table
-        $this->setForWrite();
+	protected function writeStmt(Closure $callback) {
+		// Switch the builder `from` to the write table
+		$this->setForWrite();
 
-        // Execute the statement
-        $result = $callback();
+		// Execute the statement
+		$result = $callback();
 
-        // Switch back to the read view
-        $this->setForRead();
+		// Switch back to the read view
+		$this->setForRead();
 
-        return $result;
-    }
+		return $result;
+	}
 
-    public function insert(array $values) {
-        return $this->writeStmt(fn() => parent::insert($values));
-    }
+	public function insert(array $values) {
+		return $this->writeStmt(fn() => parent::insert($values));
+	}
 
-    public function insertGetId(array $values, $sequence = null) {
-        return $this->writeStmt(fn() => parent::insertGetId($values, $sequence));
-    }
+	public function insertGetId(array $values, $sequence = null) {
+		return $this->writeStmt(fn() => parent::insertGetId($values, $sequence));
+	}
 
-    public function insertOrIgnore(array $values) {
-        return $this->writeStmt(fn() => parent::insertOrIgnore($values));
-    }
+	public function insertOrIgnore(array $values) {
+		return $this->writeStmt(fn() => parent::insertOrIgnore($values));
+	}
 
-    public function insertUsing(array $columns, $query) {
-        return $this->writeStmt(fn() => parent::insertUsing($columns, $query));
-    }
+	public function insertUsing(array $columns, $query) {
+		return $this->writeStmt(fn() => parent::insertUsing($columns, $query));
+	}
 
-    public function update(array $values) {
-        return $this->writeStmt(fn() => parent::update($values));
-    }
+	public function update(array $values) {
+		return $this->writeStmt(fn() => parent::update($values));
+	}
 
-    public function updateFrom(array $values) {
-        return $this->writeStmt(fn() => parent::updateFrom($values));
-    }
+	public function updateFrom(array $values) {
+		return $this->writeStmt(fn() => parent::updateFrom($values));
+	}
 
-    public function upsert(array $values, $uniqueBy, $update = null) {
-        return $this->writeStmt(fn() => parent::upsert($values, $uniqueBy, $update));
-    }
+	public function upsert(array $values, $uniqueBy, $update = null) {
+		return $this->writeStmt(fn() => parent::upsert($values, $uniqueBy, $update));
+	}
 
-    public function touch($column = null) {
-        return $this->writeStmt(fn() => parent::touch($column));
-    }
+	public function touch($column = null) {
+		return $this->writeStmt(fn() => parent::touch($column));
+	}
 
-    public function truncate() {
-        $this->writeStmt(fn() => parent::truncate());
-    }
+	public function truncate() {
+		$this->writeStmt(fn() => parent::truncate());
+	}
 
-    public function increment($column, $amount = 1, array $extra = []) {
-        return $this->writeStmt(fn() => parent::increment($column, $amount, $extra));
-    }
+	public function increment($column, $amount = 1, array $extra = []) {
+		return $this->writeStmt(fn() => parent::increment($column, $amount, $extra));
+	}
 
-    public function decrement($column, $amount = 1, array $extra = []) {
-        return $this->writeStmt(fn() => parent::decrement($column, $amount, $extra));
-    }
+	public function decrement($column, $amount = 1, array $extra = []) {
+		return $this->writeStmt(fn() => parent::decrement($column, $amount, $extra));
+	}
 
-    public function delete() {
-        return $this->writeStmt(fn() => parent::delete());
-    }
+	public function delete() {
+		return $this->writeStmt(fn() => parent::delete());
+	}
 
-    public function forceDelete() {
-        return $this->writeStmt(fn() => parent::forceDelete());
-    }
+	public function forceDelete() {
+		return $this->writeStmt(fn() => parent::forceDelete());
+	}
 }

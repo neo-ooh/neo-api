f7f2ffdd5010808a738e1541c538c02217db986d
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-10-04 15:26:02 -0400

    properties: Split availabilities caching in subjobs to prevent timeouts

--- Modules/Properties/Jobs/CacheProductsAvailabilitiesSubJob.php
+++ Modules/Properties/Jobs/CacheProductsAvailabilitiesSubJob.php
@@ -5,33 +5,28 @@
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
  *
- * @neo/api - CacheProductsAvailabilitiesJob.php
+ * @neo/api - CacheProductsAvailabilitiesSubJob.php
  */
 
 namespace Neo\Modules\Properties\Jobs;
 
-use Carbon\Carbon;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Database\Eloquent\Collection;
 use Illuminate\Foundation\Bus\Dispatchable;
 use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
-use Neo\Modules\Properties\Models\Product;
+use Neo\Http\Controllers\AvailabilitiesController;
 
-class CacheProductsAvailabilitiesJob implements ShouldQueue {
+class CacheProductsAvailabilitiesSubJob implements ShouldQueue {
 	use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
 
-	public function __construct() {
+	public function __construct(public Collection $products, public int $year) {
 	}
 
 	public function handle(): void {
 		// We want to cache the availabilities for all products for the current year and the next
-		Product::query()
-		       ->where("is_bonus", "=", false)
-		       ->chunk(500, function (Collection $products, $page) {
-			       CacheProductsAvailabilitiesSubJob::dispatch($products->pluck("id"), Carbon::now()->year);
-			       CacheProductsAvailabilitiesSubJob::dispatch($products->pluck("id"), Carbon::now()->year + 1);
-		       });
+		$controller = new AvailabilitiesController();
+		$controller->getAvailabilitiesForYear($this->products, $this->year);
 	}
 }

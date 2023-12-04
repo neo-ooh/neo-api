9fbe8da0c3a4a1bebb5cfe4a4cc32c1215477d05
Valentin Dufois <19367658+valentin-dufois@users.noreply.github.com>
2023-12-04 16:20:47 -0500

    access-tokens: Allow listing access tokens by a specific capability

--- app/Http/Controllers/AccessTokensController.php
+++ app/Http/Controllers/AccessTokensController.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Copyright 2023 (c) Neo-OOH - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
@@ -10,6 +10,7 @@
 
 namespace Neo\Http\Controllers;
 
+use Illuminate\Database\Eloquent\Builder;
 use Illuminate\Http\Response;
 use Neo\Http\Requests\AccessTokens\DestroyAccessTokenRequest;
 use Neo\Http\Requests\AccessTokens\ListAccessTokensRequest;
@@ -19,48 +20,55 @@ use Neo\Http\Requests\AccessTokens\UpdateAccessTokenRequest;
 use Neo\Models\AccessToken;
 
 class AccessTokensController extends Controller {
-    /**
-     * @param ListAccessTokensRequest $request
-     * @return Response
-     * @noinspection PhpUnusedParameterInspection
-     */
-    public function index(ListAccessTokensRequest $request): Response {
-        return new Response(AccessToken::all());
-    }
+	/**
+	 * @param ListAccessTokensRequest $request
+	 * @return Response
+	 * @noinspection PhpUnusedParameterInspection
+	 */
+	public function index(ListAccessTokensRequest $request): Response {
+		return new Response(AccessToken::query()
+		                               ->when($request->has("capability"), function (Builder $query) use ($request) {
+			                               $query->whereHas("capabilities", function (Builder $query) use ($request) {
+				                               $query->where("slug", "=", $request->input("capability"));
+			                               });
+		                               })
+		                               ->orderBy("name")
+		                               ->get());
+	}
 
-    /**
-     * @param ShowAccessTokenRequest $request
-     * @param AccessToken            $accessToken
-     * @return Response
-     */
-    public function show(ShowAccessTokenRequest $request, AccessToken $accessToken): Response {
-        return new Response($accessToken);
-    }
+	/**
+	 * @param ShowAccessTokenRequest $request
+	 * @param AccessToken            $accessToken
+	 * @return Response
+	 */
+	public function show(ShowAccessTokenRequest $request, AccessToken $accessToken): Response {
+		return new Response($accessToken);
+	}
 
-    public function store(StoreAccessTokenRequest $request): Response {
-        $inputs            = $request->validated();
-        $accessToken       = new AccessToken();
-        $accessToken->name = $inputs["name"];
-        $accessToken->save();
+	public function store(StoreAccessTokenRequest $request): Response {
+		$inputs            = $request->validated();
+		$accessToken       = new AccessToken();
+		$accessToken->name = $inputs["name"];
+		$accessToken->save();
 
-        $accessToken->capabilities()->attach($inputs["capabilities"]);
+		$accessToken->capabilities()->attach($inputs["capabilities"]);
 
-        return new Response($accessToken->refresh(), 201);
-    }
+		return new Response($accessToken->refresh(), 201);
+	}
 
-    public function update(UpdateAccessTokenRequest $request, AccessToken $accessToken): Response {
-        $inputs            = $request->validated();
-        $accessToken->name = $inputs["name"];
-        $accessToken->save();
+	public function update(UpdateAccessTokenRequest $request, AccessToken $accessToken): Response {
+		$inputs            = $request->validated();
+		$accessToken->name = $inputs["name"];
+		$accessToken->save();
 
-        $accessToken->capabilities()->sync($inputs["capabilities"]);
+		$accessToken->capabilities()->sync($inputs["capabilities"]);
 
-        return new Response($accessToken->refresh());
-    }
+		return new Response($accessToken->refresh());
+	}
 
-    public function destroy(DestroyAccessTokenRequest $request, AccessToken $accessToken): Response {
-        $accessToken->delete();
+	public function destroy(DestroyAccessTokenRequest $request, AccessToken $accessToken): Response {
+		$accessToken->delete();
 
-        return new Response();
-    }
+		return new Response();
+	}
 }
--- app/Http/Requests/AccessTokens/ListAccessTokensRequest.php
+++ app/Http/Requests/AccessTokens/ListAccessTokensRequest.php
@@ -1,6 +1,6 @@
 <?php
 /*
- * Copyright 2020 (c) Neo-OOH - All Rights Reserved
+ * Copyright 2023 (c) Neo-OOH - All Rights Reserved
  * Unauthorized copying of this file, via any medium is strictly prohibited
  * Proprietary and confidential
  * Written by Valentin Dufois <vdufois@neo-ooh.com>
@@ -12,26 +12,29 @@ namespace Neo\Http\Requests\AccessTokens;
 
 use Illuminate\Foundation\Http\FormRequest;
 use Illuminate\Support\Facades\Gate;
+use Illuminate\Validation\Rules\Enum;
 use Neo\Enums\Capability;
 
 class ListAccessTokensRequest extends FormRequest {
-    /**
-     * Determine if the user is authorized to make this request.
-     *
-     * @return bool
-     */
-    public function authorize() {
-        return Gate::allows(Capability::access_token_edit->value);
-    }
+	/**
+	 * Determine if the user is authorized to make this request.
+	 *
+	 * @return bool
+	 */
+	public function authorize() {
+		return Gate::allows(Capability::access_token_edit->value)
+			|| Gate::allows(Capability::dynamics_weather_edit->value)
+			|| Gate::allows(Capability::dynamics_news_edit->value);
+	}
 
-    /**
-     * Get the validation rules that apply to the request.
-     *
-     * @return array
-     */
-    public function rules() {
-        return [
-            //
-        ];
-    }
+	/**
+	 * Get the validation rules that apply to the request.
+	 *
+	 * @return array
+	 */
+	public function rules() {
+		return [
+			"capability" => ["sometimes", new Enum(Capability::class)],
+		];
+	}
 }
